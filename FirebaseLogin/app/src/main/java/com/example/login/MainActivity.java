73143ccd54f4e48a22478947f8432289bdff4e3c
lutfiikaracom <44338674+lutfiikaracom@users.noreply.github.com>
2020-01-06 22:38:07 +0200

    Update MainActivity.java

--- FirebaseLogin/app/src/main/java/com/example/login/MainActivity.java
+++ FirebaseLogin/app/src/main/java/com/example/login/MainActivity.java
@@ -3,26 +3,11 @@ package com.example.login;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActivityOptions;
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.util.Pair;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 }
